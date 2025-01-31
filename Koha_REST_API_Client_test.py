3ebdc1a7cba08b84203a60a6d6b6ef63ec551e7c
Alban Peyrat (Archi) <alban.peyrat@paris-est.archi.fr>
2025-01-31 14:53:06 +0100

    REST API CLient add getting authorities (individual or list)

--- Koha_REST_API_Client_test.py
+++ Koha_REST_API_Client_test.py
@@ -6,7 +6,8 @@ from Koha_REST_API_Client import KohaRESTAPIClient, Content_Type
 
 load_dotenv()
 
-koha = KohaRESTAPIClient(os.getenv("KOHA_TEST_URL"), os.getenv("KOHA_TEST_CLIENT_ID"), os.getenv("KOHA_TEST_CLIENT_SECRET"))
+koha = KohaRESTAPIClient(os.getenv("KOHA_URL"), os.getenv("KOHA_CLIENT_ID"), os.getenv("KOHA_CLIENT_SECRET"))
 
-record = koha.get_biblio("116741", format=Content_Type.MARCXML)
+record = koha.get_biblio("116741", format="application/marcxml+xml")
+authority_list = koha.list_auth(format=Content_Type.JSON, nb_res=50, auth_type="A610")
 print(record)
